abd04813e7fcdf648a59619792452b3282104b5a
Marta Rutkowska <marta.rutkowska@protonmail.com>
2019-03-27 11:51:45 +0100

    coin collection and coin ui added

---
+++ Platformer/Assets/Scripts/CoinBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    private UICoinScript uiCoinScript;
+
+    private void Awake()
+    {
+        uiCoinScript = GameObject.Find("CoinBar").GetComponent<UICoinScript>();
+        uiCoinScript.totalNumberOfCoins++;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            uiCoinScript.SetText();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/UICoinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UICoinScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    public int totalNumberOfCoins { get; set; }
+
+    private int numberOfCoinsCollected = 0;
+    
+
+    private void Start()
+    {
+        text.text = numberOfCoinsCollected + "/" + totalNumberOfCoins;
+    }
+
+    public void SetText()
+    {
+        numberOfCoinsCollected++;
+        text.text = numberOfCoinsCollected + "/" + totalNumberOfCoins;
+    }
+}
